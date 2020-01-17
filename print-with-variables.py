54bf2808d022f8509efd1791704787899c763f71
Muhammad Asad <masad@folio3.com>
2020-01-17 18:09:17 +0500

    Chaning character name in variables

---
+++ print-with-variables.py
@@ -0,0 +1,9 @@
+# Print the statement with variables
+
+character_name = "George"
+character_age = "45"
+
+print("There once was a man named "+ character_name)
+print("He was "+ character_age + " years old")
+print("He really liked the named "+ character_name)
+print("But did't liked being "+ character_age)
