aeed5ac1a823b963f828c5a1b69777d13461c88a
Muhammad Asad <masad@folio3.com>
2020-01-17 17:11:26 +0500

    Print Statements

---
+++ print-triangle.py
@@ -0,0 +1,7 @@
+# Print Statement
+
+print("\n---------------A simple triangle---------------\n")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|\n")
---
+++ print.py
@@ -0,0 +1,6 @@
+# Print Statements with story
+
+print("There once was a man named George")
+print("He was 70 years old")
+print("He really liked the named George")
+print("But did't liked being 70")
