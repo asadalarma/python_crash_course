6affdca0825610c3fcb18171162d6a5a464d6cc0
Muhammad Asad <masad@folio3.com>
2020-01-22 11:40:51 +0500

    Working with numbers

---
+++ numbers.py
@@ -0,0 +1,53 @@
+# Working with numbers
+
+#addition
+print(3+5)
+
+#multiplication
+print(5*5)
+
+#complicated
+print(5*5+4) 
+
+#complicated
+print(5*(5+4)) 
+
+#complicated
+print(5%3) 
+
+#my num
+my_num = 5
+print(my_num)
+
+print(str(my_num) + "hello this is my number")
+
+# absolute values
+number= -5
+print(number)
+
+# minimum number
+print(min(3,4))
+
+# maximum number
+print(max(4,8))
+
+# power number
+print(pow(4,8))
+
+# round number
+print(round(3.45))
+
+
+# ----------------------------------------------------
+
+
+# importing library
+from math import *
+# floor number
+print(floor(3.45))
+
+# floor number
+print(ceil(3.45))
+
+# square root number
+print(sqrt(4))
