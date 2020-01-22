847d2a2cbcd75ed3d9de095e874a6a79a209bd8b
Muhammad Asad <masad@folio3.com>
2020-01-22 12:00:03 +0500

    Working with input

---
+++ input.py
@@ -0,0 +1,12 @@
+# Getting input from User
+
+# If you are using python version 2 then raw_input function will be used
+#name =  raw_input("Enter your name: ")
+#print("Hello " +name+" !")
+
+
+# If you are using python version 3 then input function will be used
+
+name =  input("Enter your name: ")
+age = input("Your age: ")
+print("Hello " +name+" ! You are "+age)
