df08cdbdf1909f198c3d4116b0918997628cdc4c
Muhammad Asad <masad@folio3.com>
2020-01-23 15:46:34 +0500

    Working with lists

---
+++ lists.py
@@ -0,0 +1,15 @@
+# Lists 
+friends = ["kevin","Richerdson","Jim"]
+print(friends)
+
+# print start from backwards -1 Jim
+print(friends[-1])
+
+# print starting from third index
+friends = ["kevin","Richerdson","Jim","oscar","tom","ridar"]
+print(friends[3:])
+
+# override elements of the list
+friends = ["kevin","Richerdson","Jim","oscar","tom","ridar"]
+friends[1]="Hello"
+print(friends)
